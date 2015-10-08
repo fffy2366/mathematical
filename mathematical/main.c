e9522b442b7b0ea5be9d6df2ea3e8f5414844d2b
Frank <fengxuting@gmail.com>
2015-10-08 17:47:03 +0800

    Initial Commit

---
+++ mathematical/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  mathematical
+//
+//  Created by Frank on 15/10/8.
+//  Copyright (c) 2015年 Frank. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
